8bdb505fc9c77ec4ab9fc5e50f145a364ab79eaa
Aristotle <dkfud2121@naver.com>
2022-02-13 15:47:44 +0900

    style : 위치  및 컬러 조정

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -4,18 +4,18 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between h-[100px] shadow-lg">
-      <ul className="flex">
-        <li>
+    <nav className="flex justify-between h-[100px] shadow-lg items-center">
+      <div className="pl-8">
           <Link href={"/"}>로고</Link>
-        </li>
-        <li>
+        </div>
+      <ul className="flex p-3 font-bold">
+        <li className="p-3 hover:text-red-500">
           <Link href={"/"}>홈</Link>
         </li>
-        <li>
+        <li className="p-3 hover:text-red-500">
           <Link href={"/"}>소개</Link>
         </li>
-        <li>
+        <li className="p-3 hover:text-red-500">
           <Link href={"/"}>마이페이지</Link>
         </li>
       </ul>
