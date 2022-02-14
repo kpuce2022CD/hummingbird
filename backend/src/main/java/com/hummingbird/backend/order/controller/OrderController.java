97d3bb67a61c5d7996672c992df911621c1d29f1
kim1387 <kevink0@naver.com>
2022-02-14 22:41:47 +0900

    feat: add OrderController frame

---
+++ backend/src/main/java/com/hummingbird/backend/order/controller/OrderController.java
@@ -0,0 +1,10 @@
+package com.hummingbird.backend.order.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+}
