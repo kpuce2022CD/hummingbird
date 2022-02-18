2097c3a254fb15a6f2425b71de60a2d3252f640c
kim1387 <kevink0@naver.com>
2022-02-18 22:04:37 +0900

    feat: add Customer CustomerRepository

---
+++ backend/src/main/java/com/hummingbird/backend/user/domain/Customer.java
@@ -0,0 +1,23 @@
+package com.hummingbird.backend.user.domain;
+
+import com.hummingbird.backend.order.domain.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+}
