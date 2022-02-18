300d98dd13483bc8924d8d7f548a279f39170567
kim1387 <kevink0@naver.com>
2022-02-18 22:08:52 +0900

    add: add method find food by id

--- backend/src/main/java/com/hummingbird/backend/food/repository/FoodRepository.java
+++ backend/src/main/java/com/hummingbird/backend/food/repository/FoodRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food,Long> {
     List<Food> findByCategory_Id(@Param(value = "categoryID") Long categoryID);
+
+    Optional<Food> getFoodById(Long id);
 //    List<Food> findByMenu_Id(@Param(value = "menuID") Long menuID);
 }
