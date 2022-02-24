949ade835a41d95a84781ade4aac0066f0298b16
kim1387 <kevink0@naver.com>
2022-02-25 01:04:38 +0900

    refactor: fix Customer Entity
    기본 생성자 접근자를 protected로 제한
    token column 추가
    Customer 생성자 추가
    Dto를 entity로 바꿔 주는 함수 추가

--- backend/src/main/java/com/hummingbird/backend/user/domain/Customer.java
+++ backend/src/main/java/com/hummingbird/backend/user/domain/Customer.java
@@ -2,8 +2,8 @@ package com.hummingbird.backend.user.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hummingbird.backend.order.domain.Order;
-import lombok.Getter;
-import lombok.Setter;
+import com.hummingbird.backend.user.dto.CustomerDto;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,10 +11,14 @@ import java.util.List;
 
 @Getter @Setter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Customer {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id", nullable = false)
     private Long id;
+
+    @Column(name = "token",nullable = false)
+    private String token;
 
     @Column(name = "name",nullable = false)
     private String name;
@@ -22,4 +26,16 @@ public class Customer {
     @JsonIgnore
     @OneToMany(mappedBy = "customer")
     private List<Order> orders = new ArrayList<>();
+
+    @Builder
+    public Customer(String name) {
+        //todo add token later
+        this.token = name+"token";
+        this.name = name;
+    }
+
+    public static Customer toEntity(CustomerDto customerDto) {
+        //todo add token later
+        return new Customer(customerDto.getName());
+    }
 }
