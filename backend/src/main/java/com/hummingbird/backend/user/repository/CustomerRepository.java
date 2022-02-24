2f2e8b5fedb4340c20ccb0c2bf13803c3bf27e80
kim1387 <kevink0@naver.com>
2022-02-25 01:05:39 +0900

    feat: at findCustomerByToken method at CustomerRepository

--- backend/src/main/java/com/hummingbird/backend/user/repository/CustomerRepository.java
+++ backend/src/main/java/com/hummingbird/backend/user/repository/CustomerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    Optional<Customer> getCustomerById(Long id);
-}
+    Optional<Customer> findCustomerById(Long id);
+    Optional<Customer> findCustomerByToken(String token);}
