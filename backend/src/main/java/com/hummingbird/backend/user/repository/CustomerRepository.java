20174551e7e48cf2bf0a34ec56b0b58c4b218c8c
kim1387 <kevink0@naver.com>
2022-02-27 01:56:15 +0900

    feat: add findCustomerByEmail method

--- backend/src/main/java/com/hummingbird/backend/user/repository/CustomerRepository.java
+++ backend/src/main/java/com/hummingbird/backend/user/repository/CustomerRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     Optional<Customer> findCustomerById(Long id);
-    Optional<Customer> findCustomerByToken(String token);}
+    Optional<Customer> findCustomerByToken(String token);
+    Optional<Customer> findCustomerByEmail(String email);
+}
