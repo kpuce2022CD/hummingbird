2404d7a5967a352acbe1671786bbc316d86becfb
kim1387 <kevink0@naver.com>
2022-02-26 16:46:38 +0900

    feat: add CreateDtos for OrderCreateRequest

---
+++ backend/src/main/java/com/hummingbird/backend/order/dto/request/OrderCreateDto.java
@@ -0,0 +1,21 @@
+package com.hummingbird.backend.order.dto.request;
+
+import com.hummingbird.backend.order.domain.OrderStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class OrderCreateDto {
+
+    private Long customerId;
+
+    private Long shopId;
+
+    private OrderStatus orderStatus;
+
+    private List<OrderItemCreateDto> orderItemList;
+
+}
