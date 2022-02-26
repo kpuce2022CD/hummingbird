59271790c8534956817d1c76e903f219e1a86e1c
kim1387 <kevink0@naver.com>
2022-02-27 01:55:54 +0900

    feat: add email and password with validating

--- backend/src/main/java/com/hummingbird/backend/user/dto/CustomerDto.java
+++ backend/src/main/java/com/hummingbird/backend/user/dto/CustomerDto.java
@@ -1,23 +1,40 @@
 package com.hummingbird.backend.user.dto;
 
 import com.hummingbird.backend.user.domain.Customer;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CustomerDto {
-
-    private Long id;
-
     @NotEmpty
     private String name;
 
     @NotEmpty
     private String token;
 
+    @NotEmpty
+    @Email(regexp =  "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+            message = "유효 하지 않은 이메일 형식 입니다.")
+    private String email;
 
+    @NotEmpty
+    @Pattern(regexp = "(?=^.{8,32}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$",
+            message = "비밀 번호는 최소 1개의 대소문자, 특수문자, 숫자를 포함한 8자 이상여야 합니다.")
+    private String password;
+
+    public Customer toEntity(PasswordEncoder passwordEncoder) {
+        return Customer.builder()
+                .name(name)
+                .token(token)
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .build();
+    }
 }
