fc890dfbd6185a59a843eaf7cbf871f8c9db540a
kim1387 <kevink0@naver.com>
2022-02-27 17:04:18 +0900

    feat: add OwnerDto and fix Owner Entity

--- backend/src/main/java/com/hummingbird/backend/user/domain/Owner.java
+++ backend/src/main/java/com/hummingbird/backend/user/domain/Owner.java
@@ -3,10 +3,8 @@ package com.hummingbird.backend.user.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hummingbird.backend.order.domain.Order;
 import com.hummingbird.backend.user.dto.CustomerDto;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.hummingbird.backend.user.dto.OwnerDto;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -15,7 +13,7 @@ import java.util.List;
 @Getter @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Owner {
+public class Owner extends User{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "owner_id", nullable = false)
     private Long id;
@@ -30,14 +28,15 @@ public class Owner {
     @OneToMany(mappedBy = "customer")
     private List<Order> orders = new ArrayList<>();
 
-    public Owner(String name) {
+    @Builder
+    public Owner(String token, String name) {
         //todo add token later
         this.token = name+"token";
         this.name = name;
     }
 
-    public static Owner toEntity(CustomerDto customerDto) {
+    public static Owner toEntity(OwnerDto ownerDto) {
         //todo add token later
-        return new Owner(customerDto.getName());
+        return new Owner(ownerDto.getToken(),ownerDto.getName());
     }
 }
