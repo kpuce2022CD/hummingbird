60bdb448cda575d29d58a4ce173470501c0e6084
kim1387 <kevink0@naver.com>
2022-03-01 00:55:59 +0900

    fix: add getter method

--- backend/src/main/java/com/hummingbird/backend/order/dto/response/OrderCreateResponse.java
+++ backend/src/main/java/com/hummingbird/backend/order/dto/response/OrderCreateResponse.java
@@ -3,8 +3,10 @@ package com.hummingbird.backend.order.dto.response;
 import com.hummingbird.backend.order.domain.OrderStatus;
 import com.hummingbird.backend.order.dto.OrderInfoDto;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class OrderCreateResponse {
 
     private int tableNumber;
