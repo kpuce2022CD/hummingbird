f8c7a2c7bff0a47e07bc02d178e48c237d63f716
kim1387 <kevink0@naver.com>
2022-02-28 19:25:39 +0900

    chore: just add for create dummy json

---
+++ backend/src/main/java/com/hummingbird/backend/order/dto/request/ForCreateJSON.java
@@ -0,0 +1,33 @@
+//package com.hummingbird.backend.order.dto.request;
+//
+//import com.hummingbird.backend.food.domain.Food;
+//import com.hummingbird.backend.order.domain.Order;
+//import com.hummingbird.backend.order.domain.OrderItem;
+//import com.hummingbird.backend.order.domain.OrderStatus;
+//
+//import java.util.List;
+//
+//public class ForCreateJSON {
+//    private int tableNumber;
+//
+//    private OrderCreateDto1 orderInfoDto;
+//}
+//
+//class OrderCreateDto1 {
+//
+//    private Long customerId;
+//
+//    private Long shopId;
+//
+//    private OrderStatus orderStatus;
+//
+//    private List<OrderItemCreateDto1> orderItemList;
+//
+//}
+//class OrderItemCreateDto1 {
+//
+//    private Long foodId;
+//    private int orderPrice;
+//    private int count;
+//
+//}
