72eeee55019d0bc8ba36da9d1c0f27175918845f
kim1387 <kevink0@naver.com>
2022-03-01 05:37:45 +0900

    feat: add constructors for JPQL

--- backend/src/main/java/com/hummingbird/backend/order/dto/OrderItemInfo.java
+++ backend/src/main/java/com/hummingbird/backend/order/dto/OrderItemInfo.java
@@ -10,8 +10,19 @@ import lombok.Getter;
 @Builder
 public class OrderItemInfo {
 
-    private FoodInfo food;
+    private String foodName;
+    private int foodPrice;
     private int orderPrice;
     private int count;
 
+    public int calOrderPrice() {
+        return foodPrice * count;
+    }
+
+    public OrderItemInfo(String foodName, int foodPrice, int orderPrice, int count) {
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
 }
