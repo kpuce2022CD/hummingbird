dbf796d26a08298fd82a5a21a50074d25490714c
kim1387 <kevink0@naver.com>
2022-03-01 16:56:53 +0900

    fix: add 누락된 import

--- backend/src/main/java/com/hummingbird/backend/food/service/serviceImpl/FoodServiceImpl.java
+++ backend/src/main/java/com/hummingbird/backend/food/service/serviceImpl/FoodServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
