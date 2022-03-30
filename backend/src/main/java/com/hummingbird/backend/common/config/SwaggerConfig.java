224c0ea4b5d77c5c7bee625ccffa158ca56612c1
kim1387 <kevink0@naver.com>
2022-03-30 22:16:51 +0900

    chore: add swagger enable

--- backend/src/main/java/com/hummingbird/backend/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/hummingbird/backend/common/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.hummingbird.backend.common.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket restAPI(){
