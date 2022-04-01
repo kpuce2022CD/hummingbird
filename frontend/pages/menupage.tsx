877e4b4139a5ffa1f69c3a1746be99824fc0dd6c
min <hororonoodle@gmail.com>
2022-04-01 16:41:42 +0900

    feat : prerender 동적 라우팅 설정 제거, MockJson으로 기존 데이터를 변경

--- frontend/pages/menupage.tsx
+++ frontend/pages/menupage.tsx
@@ -19,7 +19,82 @@ interface Props {
   }[];
 }
 
-const MenuPage: NextPage<Props> = ({ foodGetData }) => {
+const foodGetData = [
+  {
+    name: "토마토 연어 구이",
+    price: 9000,
+    content:
+      "생토마토로 후레쉬한 소스를 만들어 연어스테이크와 곁들인 슈퍼푸드로 만든 건강한 요리!",
+  },
+  {
+    name: "계란 오이 정식",
+    price: 7000,
+    content:
+      "싱싱한 슬라이스 오이에 부드럽게 익은 달걀을 고소한 견과류와 곁들인 비건 정식!",
+  },
+  {
+    name: "토마토 연어 구이",
+    price: 9000,
+    content:
+      "생토마토로 후레쉬한 소스를 만들어 연어스테이크와 곁들인 슈퍼푸드로 만든 건강한 요리!",
+  },
+  {
+    name: "계란 오이 정식",
+    price: 7000,
+    content:
+      "싱싱한 슬라이스 오이에 부드럽게 익은 달걀을 고소한 견과류와 곁들인 비건 정식!",
+  },
+  {
+    name: "토마토 연어 구이",
+    price: 9000,
+    content:
+      "생토마토로 후레쉬한 소스를 만들어 연어스테이크와 곁들인 슈퍼푸드로 만든 건강한 요리!",
+  },
+  {
+    name: "계란 오이 정식",
+    price: 7000,
+    content:
+      "싱싱한 슬라이스 오이에 부드럽게 익은 달걀을 고소한 견과류와 곁들인 비건 정식!",
+  },
+  {
+    name: "토마토 연어 구이",
+    price: 9000,
+    content:
+      "생토마토로 후레쉬한 소스를 만들어 연어스테이크와 곁들인 슈퍼푸드로 만든 건강한 요리!",
+  },
+  {
+    name: "계란 오이 정식",
+    price: 7000,
+    content:
+      "싱싱한 슬라이스 오이에 부드럽게 익은 달걀을 고소한 견과류와 곁들인 비건 정식!",
+  },
+  {
+    name: "토마토 연어 구이",
+    price: 9000,
+    content:
+      "생토마토로 후레쉬한 소스를 만들어 연어스테이크와 곁들인 슈퍼푸드로 만든 건강한 요리!",
+  },
+  {
+    name: "계란 오이 정식",
+    price: 7000,
+    content:
+      "싱싱한 슬라이스 오이에 부드럽게 익은 달걀을 고소한 견과류와 곁들인 비건 정식!",
+  },
+  {
+    name: "토마토 연어 구이",
+    price: 9000,
+    content:
+      "생토마토로 후레쉬한 소스를 만들어 연어스테이크와 곁들인 슈퍼푸드로 만든 건강한 요리!",
+  },
+  {
+    name: "계란 오이 정식",
+    price: 7000,
+    content:
+      "싱싱한 슬라이스 오이에 부드럽게 익은 달걀을 고소한 견과류와 곁들인 비건 정식!",
+  },
+];
+
+const MenuPage: NextPage<Props> = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   const router = useRouter();
@@ -103,24 +178,6 @@ const MenuPage: NextPage<Props> = ({ foodGetData }) => {
     </div>
   );
 };
-
-export async function getStaticProps() {
-  try {
-    const response = await axios.get("http://localhost:3000/api/getFood");
-    const data = response.data;
-    return {
-      props: {
-        foodGetData: data,
-      },
-      fallback: false,
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      notFound: true,
-    };
-  }
-}
 
 export default MenuPage;
 
