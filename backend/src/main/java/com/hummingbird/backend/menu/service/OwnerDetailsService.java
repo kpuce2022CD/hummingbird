c6b834d4859e9725808124645f9ba2467c3241b7
kim1387 <kevink0@naver.com>
2022-04-04 01:51:44 +0900

    feat: add OwnerDetailsService for load OwnerAuthenticationContext

---
+++ backend/src/main/java/com/hummingbird/backend/menu/service/OwnerDetailsService.java
@@ -0,0 +1,6 @@
+package com.hummingbird.backend.menu.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface OwnerDetailsService extends UserDetailsService {
+}
