ae0696642f19e21ae098858b14e88c95501420d8
kim1387 <kevink0@naver.com>
2022-04-03 20:49:53 +0900

    feat: add cors properties loader

---
+++ backend/src/main/java/com/hummingbird/backend/common/config/properties/CorsProperties.java
@@ -0,0 +1,15 @@
+package com.hummingbird.backend.common.config.properties;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    private String allowedOrigins;
+    private String allowedMethods;
+    private String allowedHeaders;
+    private Long maxAge;
+}
