de03760b42ba7604f7b4397fb5db6807f7623ac6
kim1387 <kevink0@naver.com>
2022-04-04 00:31:11 +0900

    fix: add @setter for setting corsSetting

--- backend/src/main/java/com/hummingbird/backend/common/config/properties/CorsProperties.java
+++ backend/src/main/java/com/hummingbird/backend/common/config/properties/CorsProperties.java
@@ -1,9 +1,11 @@
 package com.hummingbird.backend.common.config.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
 @Getter
 @Configuration
 @ConfigurationProperties(prefix = "cors")
