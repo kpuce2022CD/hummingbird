f4c08e35b85caaacc49208cc25581ebeacdb50a7
kim1387 <kevink0@naver.com>
2022-04-03 20:50:32 +0900

    feat: add corsConfig bean and add allow cors를 검증 하는 option method api

--- backend/src/main/java/com/hummingbird/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/hummingbird/backend/common/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hummingbird.backend.common.config;
 
+import com.hummingbird.backend.common.config.properties.CorsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity // Spring Security를 활성화한다는 의미의 어노테이션
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsProperties corsProperties;
+
 
     @Override
     public void configure(WebSecurity web) throws Exception{
@@ -25,14 +36,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.csrf().disable();
+        http
+                .cors()
+                .and()
+                .formLogin().disable()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll(); //cors를 검증 하는 option 함수의 경우 별도의 filter 없이 허용
+
 
     }
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
+        corsConfiguration.setAllowedMethods(Arrays.asList(corsProperties.getAllowedMethods().split(",")));
+        corsConfiguration.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins().split(",")));
+        corsConfiguration.setMaxAge(corsConfiguration.getMaxAge());
+        corsConfiguration.setAllowCredentials(true);
+
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return corsConfigurationSource;
+    }
+
 
 }
 
