f792edd49ad8cbe5a24fc6584f4bf0cdc7c4ee31
min <hororonoodle@gmail.com>
2022-04-03 21:35:11 +0900

    feat : 추가 버튼 재활용 할 수 있도록 모듈화

---
+++ frontend/components/EditPlusBtn/EditPlusBtn.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import { ImPlus } from "react-icons/im";
+
+const EditPlusBtn = () => {
+  return (
+    <>
+      <Btn />
+    </>
+  );
+};
+
+const Btn = styled(ImPlus)`
+  background-color: white;
+  border-radius: 50%;
+  font-size: 42px;
+  padding: 12px;
+  /* #19 */
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+`;
+
+export default EditPlusBtn;
