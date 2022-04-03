47a33376079a6e0f12a634ed155154b386499289
min <hororonoodle@gmail.com>
2022-04-03 21:34:52 +0900

    feat : 메뉴판 정보 및 추가하는 admin 컴포넌트 추가

---
+++ frontend/components/AdminMenu/AdminMenu.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "styled-components";
+
+const AdminMenu = () => {
+  return (
+    <div>
+      <div>
+        <p>메뉴판 추가</p>
+        <button>추가하기</button>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default AdminMenu;
