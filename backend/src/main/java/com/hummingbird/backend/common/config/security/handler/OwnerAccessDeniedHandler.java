7e82fa7e86c9a2514fb689ff3680b103843b624c
kim1387 <kevink0@naver.com>
2022-04-04 20:57:06 +0900

    fix: error handling with handlerExceptionResolver

--- backend/src/main/java/com/hummingbird/backend/common/config/security/handler/OwnerAccessDeniedHandler.java
+++ backend/src/main/java/com/hummingbird/backend/common/config/security/handler/OwnerAccessDeniedHandler.java
@@ -1,20 +1,22 @@
 package com.hummingbird.backend.common.config.security.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class OwnerAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access is denied");
+        handlerExceptionResolver.resolveException(request, response, null, accessDeniedException);
     }
 }
