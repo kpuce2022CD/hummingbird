ca8640785e5407c42e6aa32668e3579e1a3a841d
kim1387 <kevink0@naver.com>
2022-04-04 20:41:39 +0900

    feat: add Access Denied Handler

---
+++ backend/src/main/java/com/hummingbird/backend/common/config/security/handler/OwnerAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.hummingbird.backend.common.config.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class OwnerAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access is denied");
+    }
+}
