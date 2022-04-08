be6ae72097351a80f3c082cf883f063b5c212a31
kjsong99 <mine7118@kpu.ac.kr>
2022-04-08 14:30:03 +0900

    feat - iamport payment component

---
+++ frontend/pages/paypage.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import Payment from '../components/Payment'
+
+const PayPage: NextPage = () => {
+    return <Payment/>;
+};
+
+export default PayPage;
