39b7394f44c44c02e7565a3139cef66b3c58490c
min <hororonoodle@gmail.com>
2022-05-03 22:54:09 +0900

    feat : 주문내역정보 유틸 및 비동기 패칭 커스텀 훅 추가

---
+++ frontend/data/index.ts
@@ -0,0 +1,2 @@
+export * from './IOrderInfo';
+export * from './getOrderInfo';
---
+++ frontend/utils/useAsync.ts
@@ -0,0 +1,13 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export const useAsync = <T>(
+  asyncCallback: () => Promise<T>,
+  deps: any[] = []
+): [Error | null, () => void] => {
+  const [error, setError] = useState<Error | null>(null);
+  useEffect(() => {
+    asyncCallback().catch(setError);
+  }, deps);
+  const resetError = useCallback(() => setError((notUsed) => null), []);
+  return [error, resetError];
+};
