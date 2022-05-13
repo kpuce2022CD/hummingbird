bc1cf4aa2552be082efbb62d549e47ccb14f6dea
kim1387 <kevink0@naver.com>
2022-05-13 16:01:09 +0900

    feat: 프로필 조회 api 추가

--- backend/src/main/java/com/hummingbird/backend/owner/domain/Owner.java
+++ backend/src/main/java/com/hummingbird/backend/owner/domain/Owner.java
@@ -1,7 +1,11 @@
 package com.hummingbird.backend.owner.domain;
 
 import com.hummingbird.backend.owner.dto.OwnerDto;
-import lombok.*;
+import com.hummingbird.backend.owner.dto.OwnerProfileDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -40,5 +44,13 @@ public class Owner {
                 ownerDto.getBusinessRegistrationNumber(),
                 ownerDto.getPassword()
         );
+    }
+    public OwnerProfileDto toOwnerProfileDto() {
+        return OwnerProfileDto
+                .builder()
+                .businessRegistrationNumber(getBusinessRegistrationNumber())
+                .userEmail(getEmail())
+                .userName(getName())
+                .build();
     }
 }
