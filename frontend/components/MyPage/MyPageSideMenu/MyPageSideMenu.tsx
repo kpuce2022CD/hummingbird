6310236b585ece8ef155406978bad8936efd67b9
min <hororonoodle@gmail.com>
2022-05-18 23:38:30 +0900

    feat : 로그아웃 기능 구현

--- frontend/components/MyPage/MyPageSideMenu/MyPageSideMenu.tsx
+++ frontend/components/MyPage/MyPageSideMenu/MyPageSideMenu.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import type { FC } from 'react';
 import * as S from './MyPageSideMenu.style';
+import { useRouter } from 'next/router';
 
 type MyPageSideMenuProps = {
   setAdminContent: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const MyPageSideMenu: FC<MyPageSideMenuProps> = ({ setAdminContent }) => {
+  const router = useRouter();
+  const logout = () => {
+    sessionStorage.clear();
+    router.push('/');
+  };
   return (
     <S.SideList>
       <S.SideItem>
@@ -14,10 +20,13 @@ const MyPageSideMenu: FC<MyPageSideMenuProps> = ({ setAdminContent }) => {
           <h2>사장님 정보</h2>
         </li>
         <li>
+          <p onClick={() => setAdminContent('menu')}>메뉴판</p>
+        </li>
+        <li>
           <p onClick={() => setAdminContent('profile')}>회원정보</p>
         </li>
         <li>
-          <p onClick={() => setAdminContent('menu')}>메뉴판</p>
+          <p onClick={logout}>로그아웃</p>
         </li>
       </S.SideItem>
     </S.SideList>
