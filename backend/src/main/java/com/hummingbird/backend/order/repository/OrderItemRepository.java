bde23326cd91283bda4fe5bda2f99175dcb0c828
kjsong99 <mine7118@kpu.ac.kr>
2022-05-28 22:54:03 +0900

    feat - orderItem 조회 시 start와 end 옵션을 추가

--- backend/src/main/java/com/hummingbird/backend/order/repository/OrderItemRepository.java
+++ backend/src/main/java/com/hummingbird/backend/order/repository/OrderItemRepository.java
@@ -8,6 +8,7 @@ import com.hummingbird.backend.owner.repository.OwnerRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -15,5 +16,6 @@ public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
     List<OrderItem> findAllByStatusAndOrder(OrderItemStatus status, Order order);
     List<OrderItem> findAllByOrder(Order order);
     List<OrderItem> findAllByOrder_Owner(Owner owner);
-    List<OrderItem> findAllByStatusAndOrder_Owner(OrderItemStatus status, Owner owner);
+    List<OrderItem> findAllByStatusAndOrder_OwnerAndOrder_OrderDateBetween(OrderItemStatus status, Owner owner, LocalDateTime start, LocalDateTime end);
+
 }
